f4222756700aac82c451ff6c9a07fb253c9c91ee
Adriano Santana <adriano.santana@elife.com.br>
2025-02-25 14:08:01 -0300

    add tmpl pages

--- main.go
+++ main.go
@@ -2,21 +2,37 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+const PORT_NUMBER string = ":5005"
+
+// home is the homepage handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.page.tmpl")
+}
+
+// about is the aboutpage handler
+func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.page.tmpl")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	tmplPath := fmt.Sprintf("./templates/%s", tmpl)
+	parseTemplate, _ := template.ParseFiles(tmplPath)
+
+	err := parseTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template:", err)
+		return
+	}
+}
+
 func main() {
-	http.HandleFunc(
-		"/",
-		func(w http.ResponseWriter, r *http.Request) {
-			n, err := fmt.Fprintf(w, "<html> <h1> Hello, world! </h1> </html>")
-			if err != nil {
-				panic(err)
-			}
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-			fmt.Printf("Bytes Written %d\n", n)
-		},
-	)
-
-	http.ListenAndServe(":5005", nil)
+	fmt.Printf("Starting application on port %s\n", PORT_NUMBER)
+	http.ListenAndServe(PORT_NUMBER, nil)
 }
