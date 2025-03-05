ca12139042b4d2de09637b6ac947ce64392cc5b6
Adriano Santana <adriano.santana@elife.com.br>
2025-03-05 09:00:43 -0300

    add layouts

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -8,7 +8,8 @@ import (
 
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
 	tmplPath := fmt.Sprintf("./templates/%s", tmpl)
-	parseTemplate, _ := template.ParseFiles(tmplPath)
+	baseTmplPath := fmt.Sprintf("./templates/%s", "base.layout.tmpl")
+	parseTemplate, _ := template.ParseFiles(tmplPath, baseTmplPath)
 
 	err := parseTemplate.Execute(w, nil)
 	if err != nil {
